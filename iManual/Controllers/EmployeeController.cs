8e2ea95da2813192c970ff1e317fd4840159983a
SinSarak <sinsarak010@gmail.com>
2019-07-23 00:16:32 +0700

    Employee view

--- iManual/Controllers/EmployeeController.cs
+++ iManual/Controllers/EmployeeController.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿using iManual.Models;
+using iManual.Models.ViewModels.EmployeeViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using iManual.Models.EnumBase;
 namespace iManual.Controllers
 {
     public class EmployeeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
+            var model = new ShowEmployeeViewModel();
+            model.MainCategorys = db.MainCategorys.OrderBy(p => p.Name).Where(p => p.Active == true).ToList();
+            model.SubCategorys = db.SubCategorys.Include("MainCategory").OrderBy(p => p.Name).Where(p => p.Active == true && p.MainCategory.Active == true).ToList();
+            model.Articles = db.Articles.Include("SubCategory").OrderBy(p => p.Title)
+                            .Where(p=>p.Status == (int)ArticleStatus.Active && p.PublishStatus == (int)ArticlePublishStatus.Done && p.SubCategory.Active == true).ToList();
+            return View(model);
         }
     }
 }
---
+++ iManual/Models/ViewModels/EmployeeViewModels/ShowEmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using iManual.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iManual.Models.ViewModels.EmployeeViewModels
+{
+    public class ShowEmployeeViewModel
+    {
+        public List<MainCategory> MainCategorys { get; set; }
+        public List<SubCategory> SubCategorys { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
