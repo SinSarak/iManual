61d8d08e153913b0bc9d1e3f7fb4320ad943cb7c
SinSarak <sinsarak010@gmail.com>
2019-07-24 00:58:28 +0700

    Done Employee view

--- iManual/Controllers/EmployeeController.cs
+++ iManual/Controllers/EmployeeController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using iManual.Models.EnumBase;
+using System.Net;
+using System.Data.Entity;
 namespace iManual.Controllers
 {
     public class EmployeeController : Controller
@@ -20,6 +22,39 @@ namespace iManual.Controllers
             model.Articles = db.Articles.Include("SubCategory").OrderBy(p => p.Title)
                             .Where(p=>p.Status == (int)ArticleStatus.Active && p.PublishStatus == (int)ArticlePublishStatus.Done && p.SubCategory.Active == true).ToList();
             return View(model);
+        }
+
+        public ActionResult testing()
+        {
+            return View();
+        }
+
+        public ActionResult ArticleView(int? id)
+        {
+            var model = new ShowArticleViewModel();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }else
+            {
+                if (db.Articles.SingleOrDefault(p=>p.Id == id) == null)
+                {
+                    return HttpNotFound();
+                }
+                model.Article = db.Articles.Include("SubCategory").Include(p=>p.User.UserInformation).SingleOrDefault(p=>p.Id == id);
+                model.ArticleContent = db.ArticleContents.OrderBy(p => p.Order).Where(p=>p.ArticleId == model.Article.Id).ToList();
+                
+            }
+            return View(model);
+        }
+        public FileResult ViewPDF(int? id)
+        {
+            var pdf = "";
+            if (id != null)
+            {
+                pdf = db.ArticleContents.SingleOrDefault(p=>p.Id == id).FullPath;
+            }
+            return File(pdf, "application/pdf");
         }
     }
 }
---
+++ iManual/Models/ViewModels/EmployeeViewModels/ShowArticleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using iManual.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iManual.Models.ViewModels.EmployeeViewModels
+{
+    public class ShowArticleViewModel
+    {
+        public Article Article { get; set; }
+        public List<ArticleContent> ArticleContent { get; set; }
+    }
+}
